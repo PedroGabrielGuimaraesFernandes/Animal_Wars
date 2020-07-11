73107271ceb66191aaa28242597a5c42cafb270f
Pedro Gabriel <pedrogabriel0412@gmail.com>
2020-07-11 11:09:35 -0300

    Pequenas mudanças

--- Assets/Scripts/Level Builder/Grid.cs
+++ Assets/Scripts/Level Builder/Grid.cs
@@ -2,28 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
+using PedroG.UsefulFuncs;
 
 public class Grid {
 
     public int width;
     public int height;
+    public float cellSize;
+    public Vector3 originPosition;
     public int[,] gridArray;
+    public TextMesh[,] debugGridArray;
 
-    public Grid(int width, int height)
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
+        //Definimos a largura, a altura e o tamnho da celula
         this.width = width;
         this.height = height;
-
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+        // Passamos a largura e a altura para o grid 
         gridArray = new int[width, height];
+        debugGridArray = new TextMesh[width, height];
+        //Parte de montagem do grid
         //passa por todos os elementos de colina
         for(int gridX = 0; gridX < gridArray.GetLength(0); gridX++)
         {
             //passa por todos os elementos de linha dentro da coluna
             for(int gridY = 0; gridY < gridArray.GetLength(1); gridY++)
             {
-                UtilsClass.CreateWorldText();
+                //UtilsClass.CreateWorldText(gridArray[gridX, gridY].ToString(), null, GetWorldPosition(gridX, gridY), 20, Color.white, TextAnchor.MiddleCenter);
+                //Cria dento da celula correspondente o texto e arruma ele para ficar no seu centro
+                debugGridArray[gridX, gridY] = UsefulFunctions.CreateWorldText(gridArray[gridX, gridY].ToString(), null, GetWorldPosition(gridX, gridY) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(gridX, gridY), GetWorldPosition(gridX + 1, gridY), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(gridX, gridY), GetWorldPosition(gridX, gridY + 1),Color.white, 100f);
             }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        //SetValue(2, 1, 56);
+    }
+
+    //Transforma o X e o Y do grid em um ponto do mundo
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        //Se o cellsize for 10 então tudo de 0 a 10 sera dentro do grid 0, de 10 a 20 sera no grid 1...
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    //Altera o valor de uma celula especifica para o valor mandado
+    public void SetValue(int x, int y, int value)
+    {
+        //Garante que o número é valido
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugGridArray[x, y].text = gridArray[x,y].ToString();
         }
     }
 
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if(x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
 }
--- Assets/Scripts/Level Builder/Testing.cs
+++ Assets/Scripts/Level Builder/Testing.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
 {
+
+    private Grid grid;
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(4, 2);
-    }     
-    
+        grid = new Grid(4, 2, 10f, new Vector3(0,0));        
+
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos;
+        Vector3 worldPos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;            
+            worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            //grid.SetValue(worldPos, 56);
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+
 }
