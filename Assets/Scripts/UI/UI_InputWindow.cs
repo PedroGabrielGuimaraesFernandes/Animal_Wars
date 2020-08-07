7c511c56982cc14a3856e7db10c16e628a6e4891
PedroGabriel0412 <pedrogabriel0412@gmail.com>
2020-08-07 19:54:39 -0300

    Sistema de input de texto

--- Assets/Scripts/UI/UI_InputWindow.cs
+++ Assets/Scripts/UI/UI_InputWindow.cs
@@ -4,36 +4,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using CodeMonkey.Utils;
+using CodeMonkey;
 
 public class UI_InputWindow : MonoBehaviour
-{    
-    [SerializeField]private Button confirmBtn;
-    [SerializeField] private Button cancelBtn;
+{
+    private static UI_InputWindow instance;
+
+    [SerializeField]private Button_UI confirmBtn;
+    [SerializeField] private Button_UI cancelBtn;
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TMP_InputField textInput;
 
+    public static String Letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    public static String LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    public static String HigherCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String Numbers = "0123456789";
+
     private void Start()
-    {        
-        confirmBtn = transform.Find("ConfirmBtn").GetComponent<Button>();
-        cancelBtn = transform.Find("CancelBtn").GetComponent<Button>();
+    {
+        instance = this;
+
+        confirmBtn = transform.Find("ConfirmBtn").GetComponent<Button_UI>();
+        cancelBtn = transform.Find("CancelBtn").GetComponent<Button_UI>();
         titleText = transform.Find("Titulo").GetComponent<TextMeshProUGUI>();
         textInput = transform.Find("InputField").GetComponent<TMP_InputField>();
 
         Hide();
     }
 
-    public void Show(string titleString, string inputString , Action OnCancel, Action<String> OnConfirm)
+    private void Update()
     {
-        gameObject.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            confirmBtn.ClickFunc();
+        }
 
-        titleText.text = titleString;
-        textInput.text = inputString;
-
-          //cancelBtn.onClick = () => OnConfirm(textInput.text);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cancelBtn.ClickFunc();
+        }
     }
 
-    public void Hide()
+    private  void Show(string titleString, string inputString, string validCharacters, int characterLimit, Action OnCancel, Action<String> OnConfirm)
+    {
+        gameObject.SetActive(true);
+        textInput.Select();
+        textInput.characterLimit = characterLimit;
+
+        titleText.text = titleString;
+
+        textInput.onValidateInput = (string text, int charIndex, char addedChar) => { return ValidadeChar(validCharacters, addedChar); };
+
+        confirmBtn.ClickFunc = () => {
+            Hide();
+            OnConfirm(textInput.text);
+        };
+
+        cancelBtn.ClickFunc = () =>
+        {
+            Hide();
+            OnCancel();
+        };
+    }
+
+    private void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private char ValidadeChar(string validCharacters, char addedChar)
+    {
+        if (validCharacters.IndexOf(addedChar) != -1)
+            return addedChar;// valido
+        else
+            return '\0';//Invalido  
+    }
+
+    public static void Show_Static(string titleString, string inputString, string validCharacters, int characterLimit, Action OnCancel, Action<String> OnConfirm)
+    {
+        instance.Show(titleString, inputString, validCharacters, characterLimit, OnCancel, OnConfirm);
     }
 }
--- Assets/Scripts/UI/UI_InputWindowManager.cs
+++ Assets/Scripts/UI/UI_InputWindowManager.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
 
 public class UI_InputWindowManager : MonoBehaviour
 {
-    private void Awake()
+    
+    private void Start()
     {
-
-
+        //inputWindow = GetComponent<UI_InputWindow>();
     }
 
     public void Save_Button()
     {
+        UI_InputWindow.Show_Static("Escreva o nome a ser dado ao documento", "", UI_InputWindow.Letters + UI_InputWindow.Numbers, 20, () => {
+            //Clicou no cancela
+            CMDebug.TextPopupMouse("Cancel!");
+        }, (string inputText) => {
+            //Clicou no ok
 
+            //Aqui que eu quero por a função de save
+            CMDebug.TextPopupMouse("Confirm " + inputText + "!");
+        });
     }
 
     public void Load_Button()
     {
+        UI_InputWindow.Show_Static("Escreva o nome do documento procurado", "", UI_InputWindow.Letters + UI_InputWindow.Numbers, 20, () => {
+            //Clicou no cancela
+            CMDebug.TextPopupMouse("Cancel!");
+        }, (string inputText) => {
+            //Clicou no ok
 
+            //Aqui que eu quero por a função de carregar
+            CMDebug.TextPopupMouse("Confirm " + inputText + "!");
+        });
     }
 }
